fb2b8589c19eb4c04a149e22e7bc80b6087b1420
Bill <bill@donsuhr.com>
2019-01-28 20:01:15 -0800

    get head data for src files

--- serverless/img-bucket/listImages.js
+++ serverless/img-bucket/listImages.js
@@ -3,23 +3,51 @@
 // npx serverless invoke local --function ListImages
 
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-const { returnError, returnSuccess } = require('./format-lambda-proxy-response');
+const {
+    returnError,
+    returnSuccess,
+} = require('./format-lambda-proxy-response');
 
 const s3 = new AWS.S3();
 
-function processEvent(event, context) {
+function getS3ObjectHead(Key) {
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key,
+    };
+
+    return s3.headObject(params).promise();
+}
+
+function filterFolders(x) {
+    return !x.Key.endsWith('/');
+}
+
+async function includeMetaOnSrcFiles(x) {
+    if (x.Key.startsWith('src/')) {
+        x.head = await getS3ObjectHead(x.Key);
+    }
+    return x;
+}
+
+async function processEvent(event, context) {
     // gist to include metadata
     // https://gist.github.com/mihaiserban/1f35d488405812f2bbd4b16e38e4afb5
-    return s3
+    const data = await s3
         .listObjects({
             Bucket: process.env.BUCKET,
         })
         .promise();
+    const pArray = data.Contents.filter(filterFolders).map(
+        includeMetaOnSrcFiles,
+    );
+    const items = await Promise.all(pArray);
+    return { items };
 }
 
 module.exports.handler = async (event, context) => {
-    console.log('Auth-Jwt Received event: ', JSON.stringify(event, null, 2));
-    console.log('Auth Jwt Context', JSON.stringify(context, null, 2));
+    // console.log('Received event: ', JSON.stringify(event, null, 2));
+    // console.log('Context', JSON.stringify(context, null, 2));
 
     try {
         return returnSuccess(await processEvent(event, context));
